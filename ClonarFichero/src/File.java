d1a5d55b7ee27ce0a2ee8691a21698dd47ea6ce7
Daniel <danidev@gmail.com>
2017-10-05 03:00:19 +0200

    Acabada lectura fichero

--- ClonarFichero/src/File.java
+++ ClonarFichero/src/File.java
@@ -11,16 +11,24 @@ public class File {
 		// TODO Auto-generated method stub
 		
 		try {
-			BufferedReader in = new BufferedReader(new FileReader("TextTest.txt"));
-			BufferedWriter out = new BufferedWriter(new FileWriter("TextTest2.txt"));
+			BufferedReader in = new BufferedReader(new FileReader("TextTest"));
+			BufferedWriter out = new BufferedWriter(new FileWriter("TextTest2"));
+			BufferedReader in2 = new BufferedReader(new FileReader("TextTest2"));
 				String rLine=in.readLine();
 				while (rLine!=null) {
 					out.write(rLine);
+					out.newLine();
 					rLine=in.readLine();
 					}
 				in.close();
 				out.flush();
-				out.close();				
+				out.close();
+				String rLine2=in2.readLine();
+				while(rLine2!=null) {
+					System.out.println(rLine2);
+					rLine2=in2.readLine();					
+				}	
+				in2.close();
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
