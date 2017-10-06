89dba848d834b467fea0c1d92688c7e0b46a03a5
Daniel <danidev@gmail.com>
2017-10-06 16:07:35 +0200

    Falta seleccionar el dia

--- GraficaTiempo/src/com/daniel/blanco/almacenar/Almacenar.java
+++ GraficaTiempo/src/com/daniel/blanco/almacenar/Almacenar.java
@@ -12,16 +12,35 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class Almacenar {
-	void read() throws IOException{
+	public static void main(String[] args) throws IOException {
 		try {
-			BufferedReader in=new BufferedReader(new FileReader("wrf_arw_det_history_d02_20151015_1200.txt"));
-			String dateFile = in.readLine();
-			String rLine=in.readLine();
-			while (dateFile!=null) {
-				rLine=in.readLine();
+			int i = 0;
+			DecimalFormat df=new DecimalFormat("#.00");//Damos formato de 2 digitos
+			BufferedReader in = new BufferedReader(new FileReader("wrf_arw_det_history_d02_20151015_1200.txt"));
+			String rLine = in.readLine();
+			while (rLine != null) {
+				rLine = in.readLine();//nos saltamos el enunciado
+				String[] storeValue = rLine.split(",");
+				for (int j = 0; j < storeValue.length; j++) {
+					if (j == 0 || j == 8) { //indicamos hora y temperatura
+						System.out.print(storeValue[j] + " ");
+						if (j == 8) {
+							String k=storeValue[j];
+							double c=Double.parseDouble(k);
+							c=c-273.15F;
+							System.out.print(df.format(c));
+						}
+					}
 				}
+				System.out.println();
+
+				rLine = in.readLine();
+			}
+			in.close();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
