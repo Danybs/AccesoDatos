e04bae35bc43bb3c43dc7b92122406c906a8aedd
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-06 19:35:47 +0200

    acabado

--- GraficaTiempo/src/com/daniel/blanco/almacenar/Almacenar.java
+++ GraficaTiempo/src/com/daniel/blanco/almacenar/Almacenar.java
@@ -13,31 +13,49 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Almacenar {
 	public static void main(String[] args) throws IOException {
+		String date;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Dime fecha formato año-mes-dia: ");
+		date = teclado.nextLine();
 		try {
-			int i = 0;
-			DecimalFormat df=new DecimalFormat("#.00");//Damos formato de 2 digitos
+			DecimalFormat df = new DecimalFormat("#.00");// Damos formato de 2 digitos
 			BufferedReader in = new BufferedReader(new FileReader("wrf_arw_det_history_d02_20151015_1200.txt"));
 			String rLine = in.readLine();
-			while (rLine != null) {
-				rLine = in.readLine();//nos saltamos el enunciado
+			String[] head = rLine.split(",");
+			for (int i = 0; i < head.length; i++) {
+				if (i == 0 || i == 8) {
+					System.out.print("   " + head[i] + "    ");
+				}
+				if(i>8) {
+					System.out.print("temp[unit=\"Cº\"]"  + "    ");
+				}
+			}
+			System.out.println();
+			while ((rLine = in.readLine()) != null) {
 				String[] storeValue = rLine.split(",");
 				for (int j = 0; j < storeValue.length; j++) {
-					if (j == 0 || j == 8) { //indicamos hora y temperatura
-						System.out.print(storeValue[j] + " ");
-						if (j == 8) {
-							String k=storeValue[j];
-							double c=Double.parseDouble(k);
-							c=c-273.15F;
-							System.out.print(df.format(c));
-						}
+					if (j == 0 && storeValue[j].contains(date)) { // indicamos hora
+						String formatString = storeValue[j].substring(11, 20);
+						System.out.print(formatString + "          ");
 					}
-				}
-				System.out.println();
+					if (j == 8 && storeValue[0].contains(date)) {// indicamos temperatura en Kelvin
+						String c = storeValue[j];
+						double c1 = Double.parseDouble(c);
+						System.out.print(df.format(c1) + "          ");
+					}
+					if (j == 8 && storeValue[0].contains(date)) {// indicamos temperatura en Celsius
+						String k = storeValue[j];
+						double c = Double.parseDouble(k);
+						c = c - 273.15F;
+						System.out.print(df.format(c));
+						System.out.println();
+					}
 
-				rLine = in.readLine();
+				}
 			}
 			in.close();
 
