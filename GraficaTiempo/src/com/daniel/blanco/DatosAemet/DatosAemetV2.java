e42c34f6039c1fbfb0826228a7b0c49d44e2dca3
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-13 19:39:04 +0200

    v2

--- GraficaTiempo/src/com/daniel/blanco/DatosAemet/DatosAemetV2.java
+++ GraficaTiempo/src/com/daniel/blanco/DatosAemet/DatosAemetV2.java
@@ -9,15 +9,18 @@ package com.daniel.blanco.DatosAemet;
  */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 
-public class DatosAemet {
+public class DatosAemetV2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
@@ -33,47 +36,62 @@ public class DatosAemet {
 
 			// Se obtiene el inputStream de la foto web y se abre el fichero
 			// local
-			InputStream is = urlCon.getInputStream();
+			BufferedReader is = new BufferedReader(new InputStreamReader(url.openStream()));
 
-			FileOutputStream fos = new FileOutputStream("datosAemet.csv",true);
-			//BufferedReader readC = new BufferedReader(new FileReader("datosAemet.csv"));
+			BufferedWriter writeO = new BufferedWriter(new FileWriter("datosAemet.csv",true));
+			BufferedReader readC = new BufferedReader(new FileReader("datosAemet.csv"));
 			// Lectura del CSV y escritura en local
-			byte[] array = new byte[1000]; // buffer temporal de lectura
-			int leido = is.read(array);
-			while (leido > 0) {
-				fos.write(array, 0, leido);
-				leido = is.read(array);
+			String ReadO = is.readLine();
+			String ReadCO = readC.readLine();
+			Boolean check=false;
+			while (is.readLine() != null) {	
+				writeO.write(ReadO);
+				writeO.newLine();
+				readO=is.readLine();
+				
+				/*while (readC.readLine()!=null) {
+					if (ReadO.equals(ReadCO)) {
+						check=true;
+					}
+				}
+				if(!check) {
+					
+				}*/
 			}
+			
+			
 
 			// cierre de conexion y fichero
 			is.close();
-			fos.close();
+			writeO.close();
+			readC.close();
+			writeO.flush();
 
 			// lectura del fichero
 			BufferedReader read = new BufferedReader(new FileReader("datosAemet.csv"));
-			String line = read.readLine();//Lectura ciudad
+			String line = read.readLine();// Lectura ciudad
 			String[] ciudad = line.split("\"");
 			for (int i = 0; i < ciudad.length; i++) {
 				System.out.println(ciudad[i]);
 			}
-			line = read.readLine();//Lectura update
+			line = read.readLine();// Lectura update
 			System.out.println(line);
-			line = read.readLine();//Espacio en blanco	
+			line = read.readLine();// Espacio en blanco
 			System.out.println(line);
-			line = read.readLine();//Lectura cabecera
+			line = read.readLine();// Lectura cabecera
 			String[] head = line.split(",");
 			for (int i = 0; i < head.length; i++) {
-				System.out.print(head[i]+" ");
+				System.out.print(head[i] + " ");
 			}
 			System.out.println();
 			ArrayList<String> aL = new ArrayList<String>();
-			while((line=read.readLine())!=null){
+			while ((line = read.readLine()) != null) {
 				aL.add(line);
 			}
-			for (int i = aL.size()-1; i>0; i--) {
+			for (int i = aL.size() - 1; i > 0; i--) {
 				System.out.println(aL.get(i));
 			}
-		
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
