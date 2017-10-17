f14499ebd9508477cbf72bb68649e68af9566475
Danybs <32314105+Danybs@users.noreply.github.com>
2017-10-17 17:42:42 +0200

    V2.1

--- GraficaTiempo/src/com/daniel/blanco/DatosAemet/DatosAemetV2.java
+++ GraficaTiempo/src/com/daniel/blanco/DatosAemet/DatosAemetV2.java
@@ -9,19 +9,18 @@ package com.daniel.blanco.DatosAemet;
  */
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class DatosAemetV2 {
 	public static void main(String[] args) {
@@ -31,90 +30,53 @@ public class DatosAemetV2 {
 			URL url = new URL(
 					"http://www.aemet.es/es/eltiempo/observacion/ultimosdatos_1249X_datos-horarios.csv?k=ast&l=1249X&datos=det&w=0&f=temperatura&x=h24");
 			Path path = Paths.get("datosAemet.csv");
-
 			// establecemos conexion
 			URLConnection urlCon = url.openConnection();
 
 			// sacamos por pantalla el tipo de fichero
 			System.out.println(urlCon.getContentType());
 
+			//Si el fichero no existe lo creamos
+			if(Files.notExists(path)) {
 			// Se obtiene el inputStream de la foto web y se abre el fichero
 			// local
-			BufferedReader is = new BufferedReader(new InputStreamReader(url.openStream()));
-			BufferedWriter writeO = new BufferedWriter(new FileWriter("datosAemet.csv",true));
-			BufferedReader readC = new BufferedReader(new FileReader("datosAemet.csv"));
-			// Lectura del CSV y escritura en local						
-			if (Files.exists(path)) {
-				String host = is.readLine();
-				String local;
-				Boolean check = true;
-				while (host != null) {	
-					local = readC.readLine();
-					while (local != null) {
-						if (host.equals(local) && !host.contains("Oviedo") && !host.contains("Fecha y hora") && !host.contains("Actualizado: ") && !host.contains("\\s+")) {
-							check = false;
-						}
-						local = readC.readLine();
-					}
-					if (check==true) {
-						writeO.write(host);
-						writeO.newLine();
-						
-						check = false;
-					}
-					host = is.readLine();
-					readC.reset();
-				}
+			InputStream is = urlCon.getInputStream();
+			FileOutputStream fos = new FileOutputStream("datosAemet.csv");
+			// Lectura del CSV y escritura en local
+			byte[] array = new byte[1000]; // buffer temporal de lectura
+			int leido = is.read(array);
+			while (leido > 0) {
+				fos.write(array, 0, leido);
+				leido = is.read(array);
 			}
-			else {
-				InputStream is1 = urlCon.getInputStream();
-				FileOutputStream fos = new FileOutputStream("datosAemet.csv");
-				//BufferedReader readC = new BufferedReader(new FileReader("datosAemet.csv"));
-				// Lectura del CSV y escritura en local
-				byte[] array = new byte[1000]; // buffer temporal de lectura
-				int leido = is1.read(array);
-				while (leido > 0) {
-					fos.write(array, 0, leido);
-					leido = is1.read(array);
-				}
-				fos.close();
-				is1.close();
-			}
+
 			// cierre de conexion y fichero
 			is.close();
+			fos.close();
+			}
+			//Leemos los nuevos datos y lo almacenamos en el treeset, como no se pueden repetir datos no se almacenaran.
 			
-			writeO.close();
-			writeO.flush();
-			readC.close();
-
-			// cierre de conexion y fichero
-			is.close();
-
 			// lectura del fichero
 			BufferedReader read = new BufferedReader(new FileReader("datosAemet.csv"));
-			String line = read.readLine();// Lectura ciudad
-			String[] ciudad = line.split("\"");
-			for (int i = 0; i < ciudad.length; i++) {
-				System.out.println(ciudad[i]);
-			}
-			line = read.readLine();// Lectura update
+			String line = read.readLine();//Lectura ciudad
 			System.out.println(line);
-			line = read.readLine();// Espacio en blanco
+			line = read.readLine();//Lectura ciudad
 			System.out.println(line);
-			line = read.readLine();// Lectura cabecera
-			String[] head = line.split(",");
-			for (int i = 0; i < head.length; i++) {
-				System.out.print(head[i] + " ");
+			line = read.readLine();//Lectura ciudad
+			System.out.println(line);
+			line = read.readLine();//Lectura ciudad
+			System.out.println(line);
+			TreeSet<String> tS = new TreeSet<String>();
+			while((line=read.readLine())!=null){
+				tS.add(line);
 			}
-			System.out.println();
-			ArrayList<String> aL = new ArrayList<String>();
-			while ((line = read.readLine()) != null) {
-				aL.add(line);
+			Iterator<String> it = tS.iterator();
+			String a;
+			while(it.hasNext()) {
+				a=(String)it.next();
+				System.out.println(a);						
 			}
-			for (int i = aL.size() - 1; i > 0; i--) {
-				System.out.println(aL.get(i));
-			}
-
+		
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
